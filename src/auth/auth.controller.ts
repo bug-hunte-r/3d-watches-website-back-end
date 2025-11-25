529ef473a01f92b8716978f9462d03a5f0f6405c
shayan <shayanjafarnejad75@gmail.com>
2025-11-25 12:32:19 +0330

    create project

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAuthDto } from './dto/create-auth.dto';
+
+@Injectable()
+export class AuthService {
+}
