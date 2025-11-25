27b32bdd93bfcac3bd8e7d81679ad63d75ccb7de
shayan <shayanjafarnejad75@gmail.com>
2025-11-25 13:48:56 +0330

    develop auth helper

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from 'src/models/User';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
+    constructor(@InjectModel(User.name) private UserModel: Model<UserDocument>) { }
+
+    async Signup (createUserDto: CreateAuthDto) {
+
+        const isUserExist = await this.UserModel.findOne({username: createUserDto.username})
+
+        if (isUserExist) {
+            throw new ConflictException('This username is already exist')
+        }
+    }
 }
---
+++ src/config/auth-helper.ts
@@ -0,0 +1,30 @@
+import { compare, hash } from "bcryptjs"
+import { sign, verify } from "jsonwebtoken"
+
+const hashPassHandler = async (password: any) => {
+    const hashedPass = hash(password, 12)
+    return hashedPass
+}
+
+const tokenGenerator = (data: any) => {
+    const token = sign({...data}, process.env.PRIVATE_KEY, {
+        expiresIn: '48h'
+    })
+    return token
+}
+
+const verifyPassHandler = async (password: any, hashedPass: any) => {
+    const verifedPass = compare(password, hashedPass)
+    return verifedPass
+}
+
+const verifyTokenHandler = async (token: any) => {
+    try {
+        const verifedToken = verify(token, process.env.privatKey)
+        return verifedToken
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export { hashPassHandler, tokenGenerator, verifyPassHandler, verifyTokenHandler }
