f54555b539274246fb9edba0031c6b482e43c38b
shayan <shayanjafarnejad75@gmail.com>
2025-11-26 10:09:08 +0330

    develop dto for update product

--- src/product/dto/update-product.dto.ts
+++ src/product/dto/update-product.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+    title: string;
+
+    desc: string;
+
+    price: number;
+
+    count: number;
+}
