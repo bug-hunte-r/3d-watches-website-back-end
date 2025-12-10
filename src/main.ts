e39bf8b6c723f9002503178c5b8c33a37fec3cb3
shayan <shayanjafarnejad75@gmail.com>
2025-12-10 17:52:52 +0330

    fix the cros error for 100th

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,14 @@ async function bootstrap() {
     serveRoot: '/uploads/models',
   });
   app.enableCors({
-    origin: ['https://3d-watch-app.netlify.app'],
-    credentials: true
-  })
+    origin: [
+      'http://localhost:3000',
+      'https://3d-watch-app.netlify.app'
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization'
+  });
   app.use('/uploads/images', Express.static(join(__dirname, '..', 'uploads/images')));
   await app.listen(process.env.PORT ?? 3000);
 }
