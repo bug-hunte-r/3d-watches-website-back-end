d46f5e88626be1d072c620554359b66d09b5124b
shayan <shayanjafarnejad75@gmail.com>
2025-12-11 20:17:01 +0330

    set application/json for cros

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     ],
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Authorization, application/json'
+    allowedHeaders: 'Content-Type, application/json'
   });
   app.use('/uploads/images', Express.static(join(__dirname, '..', 'uploads/images')));
   await app.listen(process.env.PORT ?? 3000);
